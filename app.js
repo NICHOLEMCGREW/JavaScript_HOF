6e2f922c964d8c3e1b2653b1a9b5977d44aa59ee
Nichole McGrew <nicholemcgrew@gmail.com>
2022-01-17 16:42:08 -0600

    initial commit

--- app.js
+++ app.js
@@ -2,3 +2,10 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+let plus15 = plus(15);
+function plus(number) {
+    return (plusNumber) => (plusNumber += number);
+}
+
+console.log(plus15(10));
